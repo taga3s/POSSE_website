088081c0890256ef5762092b4533a89297514fee
seiya_tagami <bepozurapu274@gmail.com>
2023-06-13 08:59:59 +0900

    chore(service): setting up esling and prettier

---
+++ service/.eslintrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2022: true,
+    node: true
+  },
+  extends: ['standard-with-typescript', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+  }
+}
