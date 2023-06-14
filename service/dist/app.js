5b5a9f63befa2aae2359c247496351f20d7751c4
seiya_tagami <bepozurapu274@gmail.com>
2023-06-15 06:31:20 +0900

    chore(service): allowing cors

--- service/dist/app.js
+++ service/dist/app.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import cors from 'cors'
 import Quiz from './controllers/quiz.js'
 const app = express()
 const port = 8080
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.listen(port, () => console.log(`Server is running at http://localhost:${port}`))
