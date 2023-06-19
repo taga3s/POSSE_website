236d1d74c9ec6a3cc9036c1987ea0e89b67d45a5
seiya_tagami <bepozurapu274@gmail.com>
2023-06-20 00:43:25 +0900

    feat(manager): deleting quiz

--- manager/src/components/features/Quizzes/TableTr.vue
+++ manager/src/components/features/Quizzes/TableTr.vue
@@ -1,26 +1,40 @@
+<script setup lang="ts">
+import { TQuiz } from '../../../types'
+
+const props = defineProps<{ quiz: TQuiz }>()
+
+const emits = defineEmits(['onClickDelete'])
+const handleDelete = (id: number) => {
+  emits('onClickDelete', id)
+}
+</script>
+
 <template>
   <tr>
-    <td
-      class="text-center py-[10px] border-y border-l border-t-0 border-table align-middle"
-    >
-      1
+    <td class="text-center py-[10px] border-y border-l border-t-0 border-table align-middle">
+      {{ props.quiz.id }}
     </td>
     <td class="text-center py-[10px] border border-t-0 border-table">
       <div class="flex justify-between">
-        <span class="text-left px-4">aa</span>
+        <span class="text-left px-4">{{ props.quiz.quiz_text }}</span>
         <ul class="flex gap-8 px-4 items-center">
           <li>
-            <router-link to="/edit" title="edit" class="text-green-600 transition-all duration-200">
+            <router-link
+              :to="`/edit?id=${props.quiz.id}`"
+              title="edit"
+              class="text-green-600 transition-all duration-200"
+            >
               <i class="fa-regular fa-pen-to-square"></i>
             </router-link>
           </li>
           <li>
-            <a
-              href=""
+            <button
               title="delete"
               class="text-red-500 transition-all duration-200"
-              ><i class="fa-sharp fa-solid fa-trash"></i
-            ></a>
+              @click="handleDelete(props.quiz.id)"
+            >
+              <i class="fa-sharp fa-solid fa-trash"></i>
+            </button>
           </li>
         </ul>
       </div>
